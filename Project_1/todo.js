74955fe0625117dc439b77c51f8136786e76ead3
yjm07 <yjm9507@yonsei.ac.kr>
2021-02-16 19:05:06 +0900

    feat: todo.js
    Make list of the inputs and print out with a button beside.
    Save the input in local storage as an object with id.

---
+++ Project_1/todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODO_LS = 'toDo';
+const toDoArr = [];
+
+function saveToDo() {
+    localStorage.setItem(TODO_LS, JSON.stringify(toDoArr));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newID = toDoArr.length + 1;
+    delBtn.innerText = "delete";
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+    li.id = newID;
+
+    const toDoObj = {
+        text: text,
+        id: newID
+    };
+    toDoArr.push(toDoObj);
+    saveToDo();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDo() {
+    const toDo = localStorage.getItem(TODO_LS);
+    if(toDo != null) {
+        const parsedToDo = JSON.parse(toDo);
+        parsedToDo.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
+    }
+}
+
+function init() {
+    loadToDo();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+init();
